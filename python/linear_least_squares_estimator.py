38512cd82d9a5b753dcfe53d4832f5141700541e
vinaybenny <vinaybenny@gmail.com>
2017-09-08 23:41:38 +1200

    Added comments

--- python/linear_least_squares_estimator.py
+++ python/linear_least_squares_estimator.py
@@ -5,15 +5,15 @@ import tensorflow as tf
 import numpy as np
 
 
-# Define x as covariates and y as dependent variable
+# Define x as covariates and y as dependent variable from input matrix
 x = tf.placeholder(tf.float32, shape = (xval.shape[0],xval.shape[1]))
 y = tf.placeholder(tf.float32, shape = (xval.shape[0],1))
 
-# Define linear least squares estimator
+# Define linear least squares estimator using Moore Penrose pesudoinverse- ((X'X)^-1)X'y 
 inverse = tf.matrix_inverse( tf.matmul( tf.transpose(x), x ) )
 b_est = tf.matmul(tf.matmul( inverse , tf.transpose(x)), y)
 
-# Get the std error of the beta estimator.
+# Get the std error of the beta estimator.  (sigma^2)*((X'X)^-1)
 sum_sq_resid = tf.reduce_sum(tf.square(tf.subtract(y, tf.matmul(x, b_est))), axis = 0)
 b_stderr = tf.diag_part(tf.sqrt(tf.multiply(sum_sq_resid, inverse)))
 
